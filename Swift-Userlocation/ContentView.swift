346a237ee5afa96206807cbe4a1afb845708a942
yusufbaltamegacode <98096643+yusufbaltamegacode@users.noreply.github.com>
2022-08-04 11:39:11 +0300

    Initial Commit

---
+++ Swift-Userlocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-Userlocation
+//
+//  Created by Yusuf Balta on 4.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-Userlocation/Swift_UserlocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UserlocationApp.swift
+//  Swift-Userlocation
+//
+//  Created by Yusuf Balta on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UserlocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
