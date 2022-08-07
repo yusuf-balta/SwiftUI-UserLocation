324ef4a7735e7bd6659fdb470f8edf518764af53
yusufbaltamegacode <98096643+yusufbaltamegacode@users.noreply.github.com>
2022-08-07 11:24:27 +0300

    first commit

---
+++ Swift-Userlocation/Map/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Swift-Userlocation
+//
+//  Created by Yusuf Balta on 4.08.2022.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ Swift-Userlocation/Map/MapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewModel.swift
+//  Swift-Userlocation
+//
+//  Created by Yusuf Balta on 4.08.2022.
+//
+
+import Foundation
